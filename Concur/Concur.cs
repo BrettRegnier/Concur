3d3e6d0f2ce2a260b8cdec251f9d1ebc425be6a1
Brett Regnier <bretternestregnier@gmail.com>
2019-01-20 16:53:44 -0700

    intial commit and working on some classes

---
+++ Concur/Concur.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Concur
+{
+	public partial class Concur : Form
+	{
+
+		public Concur()
+		{
+			InitializeComponent();
+
+			Button btn = new Button();
+			btn.Left = 10;
+			btn.Top = 10;
+			btn.Width = 100;
+			btn.Height = 60;
+			btn.Click += SyncFolders;
+		}
+
+		public void SyncFolders(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
---
+++ Concur/Folder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concur
+{
+	public class Folder
+	{
+		string name;
+		List<string> files;
+		List<string> folders;
+
+		public Folder(string nm)
+		{
+			name = nm;
+			files = new List<string>();
+			folders = new List<string>();
+		}
+
+		public string Name()
+		{
+			return name;
+		}
+
+		public List<string> Files()
+		{
+			if (files.Count == 0)
+			{
+				// Get files
+			}
+
+			return files;
+		}
+
+		public List<string> Folders()
+		{
+			if (folders.Count == 0)
+			{
+				// Get folders
+			}
+
+			return folders;
+		}
+
+		public Dictionary<string, List<string>> Contents()
+		{
+			Dictionary<string, List<string>> dic = new Dictionary<string, List<string>>
+			{
+				{ "files", Files() },
+				{ "Folders", Folders() }
+			};
+
+			return dic;
+		}
+
+	}
+}
